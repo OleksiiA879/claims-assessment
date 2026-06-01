e2964a7fe021091aa08c94fd24a1cbbce0c0e7d9
oleksiiastrakhantsev <oleksii.astrakhantsev@lemmi.io>
2026-06-01 11:47:17 +0300

    Updated .NET version

--- src/ClaimsModule.Persistence/Migrations/20260531193629_InitialCreate.Designer.cs
+++ src/ClaimsModule.Persistence/Migrations/20260531193629_InitialCreate.Designer.cs
@@ -20,7 +20,7 @@ namespace ClaimsModule.Persistence.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.11")
+                .HasAnnotation("ProductVersion", "9.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
